c5fa0df3e2525e00b3951c267fe2b592c92d32a4
ali hatem <alihatem7201737@gmail.com>
2023-02-01 21:14:28 +0200

    new updates

---
+++ Donations_App/Donations_App/Migrations/20230201190215_initial6.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DonationsApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial6 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
